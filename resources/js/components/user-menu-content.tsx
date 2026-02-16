14eba2c3ff7301b586426a494764dc12da42b76b
João Vitor <joaovitors@live.com>
2026-02-16 02:56:39 -0300

    fix: sidebar translation

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -41,7 +41,7 @@ export function UserMenuContent({ user }: Props) {
                         onClick={cleanup}
                     >
                         <Settings className="mr-2" />
-                        Settings
+                        Configurações
                     </Link>
                 </DropdownMenuItem>
             </DropdownMenuGroup>
@@ -55,7 +55,7 @@ export function UserMenuContent({ user }: Props) {
                     data-test="logout-button"
                 >
                     <LogOut className="mr-2" />
-                    Log out
+                    Sair
                 </Link>
             </DropdownMenuItem>
         </>
